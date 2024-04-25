40f4b2950c82bd0db1bcbde8d06f90e68c3b6882
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2024-04-25 09:57:04 +0530

    moved application property file

--- playlist/src/main/java/my/playlist/PlaylistApplication.java
+++ playlist/src/main/java/my/playlist/PlaylistApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlaylistApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.location", "file:/C:/Users/Gnanesh/Myplaylist/Myplaylist/playlist/src/main/java/my/playlist/application.yaml");
 		SpringApplication.run(PlaylistApplication.class, args);
 	}
 
