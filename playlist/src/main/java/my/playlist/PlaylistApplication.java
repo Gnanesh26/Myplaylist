dc234ea4e63bcc5d440f6a24a9318af60a657ba1
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2024-04-25 11:27:04 +0530

    added application properties file in cloudinary and removed in application

--- playlist/src/main/java/my/playlist/PlaylistApplication.java
+++ playlist/src/main/java/my/playlist/PlaylistApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlaylistApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.config.location", "file:/C:/Users/Gnanesh/Myplaylist/Myplaylist/playlist/src/main/java/my/playlist/application.yaml");
+		System.setProperty("spring.config.location", "https://res.cloudinary.com/dwvzgfeyd/raw/upload/v1714024017/application_goji8v_h4bgeb.yaml");
+
+//		System.setProperty("spring.config.location", "file:/C:/Users/Gnanesh/Myplaylist/Myplaylist/playlist/src/main/java/my/playlist/application.yaml");
 		SpringApplication.run(PlaylistApplication.class, args);
 	}
 
